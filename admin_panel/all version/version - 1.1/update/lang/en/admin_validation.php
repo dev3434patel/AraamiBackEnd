ac113621643a7c09fa749dddbd7e8e937d3fa748
dev3434patel <dev3434patel@gmail.com>
2024-10-03 18:22:08 +0530

    initial

---
+++ admin_panel/all version/version - 1.1/update/lang/en/admin_validation.php
@@ -0,0 +1,5 @@
+<?php
+ return array (
+  'Database update successfully' => 'Database update successfully'
+);
+ ?>
---
+++ admin_panel/all version/version - 1.2/update/lang/en/admin.php
@@ -0,0 +1,21 @@
+<?php
+ return array (
+  'Product Import' => 'Product Import',
+  'Export Product List' => 'Export Product List',
+  'Demo Export' => 'Demo Export',
+  'Product Bulk Import' => 'Product Bulk Import',
+  'Product List' => 'Product List',
+  'Export Product List' => 'Export Product List',
+  'Demo Export' => 'Demo Export',
+  'Import File' => 'Import File',
+  'Bulk Upload' => 'Bulk Upload',
+  'Export Country List' => 'Export Country List',
+  'Country List' => 'Country List',
+  'Id' => 'Id',
+  'State List' => 'State List',
+  'Export State List' => 'Export State List',
+  'City List' => 'City List',
+  'Export City List' => 'Export City List',
+  'Export Shipping Rule' => 'Export Shipping Rule',
+);
+ ?>
